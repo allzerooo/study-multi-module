e12a100f2e4d991b62c268cb16cf063be6e4a830
ddalam <dychoi.dev@gmail.com>
2022-09-24 15:20:19 +0900

    API 모듈에서 Common 모듈의 Spring Bean을 사용 컴포넌트 스캔 대상 패키지를 명시적으로 지정

--- api-module/src/main/java/study/spring/apimodule/ApiModuleApplication.java
+++ api-module/src/main/java/study/spring/apimodule/ApiModuleApplication.java
@@ -1,9 +1,11 @@
-package study.spring;
+package study.spring.apimodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"study.spring.apimodule", "study.spring.commonmodule"}
+)
 public class ApiModuleApplication {
 
 	public static void main(String[] args) {
